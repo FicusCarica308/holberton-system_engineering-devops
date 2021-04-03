3a416dfb315d1fc5087af158418b622aaaba0235
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-03 20:51:10 +0000

    I dont know why

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# This manifest will edit the ~/etc/ssh/ssh_config file and add these lines if not already there
+file_line {'Turn off passwd auth':
+    path  => '/etc/ssh/ssh_config',
+    match => '^PasswordAuthentication yes',
+    line  => 'PasswordAuthentication no',
+}
+file_line {'Declare identity file':
+    path  => '/etc/ssh/ssh_config',
+    match => '^IdentityFile',
+    line  => 'IdentityFile ~/.ssh/holberton',
+}
