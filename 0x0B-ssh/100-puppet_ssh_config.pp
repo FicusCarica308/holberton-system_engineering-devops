a0e30c3ac1b7f7fc630a8bb8b086ddfa57b7a0e4
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-03 20:29:55 +0000

    added advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,9 @@
 # This manifest will edit the ~/etc/ssh/ssh_config file and add these lines if not already there
 
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
+
 file_line{'Turn off passwd auth':
     path    => '/etc/ssh/ssh_config',
     match   => '^PasswordAuthentication yes',
