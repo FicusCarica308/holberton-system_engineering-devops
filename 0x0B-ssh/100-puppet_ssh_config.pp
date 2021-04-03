98a9df2bb8235f6965c652c33a62c6eb4ec409c9
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-03 20:34:09 +0000

    added puppet-lint fixes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,11 @@
 # This manifest will edit the ~/etc/ssh/ssh_config file and add these lines if not already there
-
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-}
-
 file_line{'Turn off passwd auth':
-    path    => '/etc/ssh/ssh_config',
-    match   => '^PasswordAuthentication yes',
-    line    => 'PasswordAuthentication no',
+    path  => '/etc/ssh/ssh_config',
+    match => '^PasswordAuthentication yes',
+    line  => 'PasswordAuthentication no',
 }
 file_line{'Declare identity file':
-    path    => '/etc/ssh/ssh_config',
-    match   => '^IdentityFile',
-    line    => 'IdentityFile ~/.ssh/holberton',
+    path  => '/etc/ssh/ssh_config',
+    match => '^IdentityFile',
+    line  => 'IdentityFile ~/.ssh/holberton',
 }
