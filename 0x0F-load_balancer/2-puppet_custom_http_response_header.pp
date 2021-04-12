657805b892c1465d03a2caaa4d796b6c19c7a731
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-12 20:52:39 +0000

    added advanced task fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,7 @@
 # This manifest handles the task of creating a custom HTTP header response
 file_line {'Cutsom http response':
     path  => '/etc/nginx/sites-available/default',
-    after => ":80 default_server;",
+    after => ':80 default_server;',
     line  => "\tadd_header X-Served-By ${hostname};",
 }
+# listen [::]:80 default_server ipv6only=on;
