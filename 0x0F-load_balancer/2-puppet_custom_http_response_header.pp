88ab35bc73ba0561ffbf34dc5c5dc6c06594209b
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-13 00:31:12 +0000

    added require check

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,17 @@
 # This manifest handles the task of creating a custom HTTP header response
-$commands = 'apt-get -y update; apt-get -y install nginx; ufw allow \'Nginx HTTP\''
+$commands = 'sudo apt-get -y update; sudo apt-get -y install nginx'
 exec {'Install nginx':
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
     command => $commands
 }
 file_line {'Http header':
-    path  => '/etc/nginx/sites-available/default',
-    after => ':80 default_server;',
-    line  => "\tadd_header X-Served-By ${hostname};",
+    path    => '/etc/nginx/sites-available/default',
+    after   => ':80 default_server;',
+    line    => "\tadd_header X-Served-By ${hostname};",
+    require => Exec['Install nginx'],
 }
 exec {'Restart nginx':
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-    command => 'sudo service nginx restart'
+    command => 'sudo service nginx restart',
+    require => Exec['Install nginx'],
 }
