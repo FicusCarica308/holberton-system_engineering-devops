0ada6558cb4a1a9f6636249fe5bff92a7b5885f7
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-13 00:27:24 +0000

    trying new approach

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,15 @@
 # This manifest handles the task of creating a custom HTTP header response
-package {'Install nginx':
-    name   => 'nginx',
-    ensure => installed,
+$commands = 'apt-get -y update; apt-get -y install nginx; ufw allow \'Nginx HTTP\''
+exec {'Install nginx':
+    path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+    command => $commands
 }
 file_line {'Http header':
     path  => '/etc/nginx/sites-available/default',
     after => ':80 default_server;',
     line  => "\tadd_header X-Served-By ${hostname};",
 }
-service {'nginx server status':
-    name   => 'nginx',
-    ensure => running,
-    enable => true,
+exec {'Restart nginx':
+    path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+    command => 'sudo service nginx restart'
 }
-# listen [::]:80 default_server ipv6only=on;
