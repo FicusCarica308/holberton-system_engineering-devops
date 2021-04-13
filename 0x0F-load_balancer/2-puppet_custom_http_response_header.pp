df6ab49284adf94534c560a7c1ea9eedb7d9ff3c
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-04-13 00:07:56 +0000

    added nginx installation and status resources

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,16 @@
 # This manifest handles the task of creating a custom HTTP header response
-
-package{ 'nginx-core':
-  ensure => installed,
+package {'Install nginx':
+    name   => 'nginx',
+    ensure => installed,
 }
-
 file_line {'Http header':
     path  => '/etc/nginx/sites-available/default',
     after => ':80 default_server;',
     line  => "\tadd_header X-Served-By ${hostname};",
 }
-
- service { 'nginx':
-   enable => true,
-   ensure => running,
-   hasrestart => true,
- }
+service {'nginx server status':
+    name   => 'nginx',
+    ensure => running,
+    enable => true,
+}
 # listen [::]:80 default_server ipv6only=on;
