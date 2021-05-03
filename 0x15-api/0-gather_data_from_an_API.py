6b984ca05bda230551ebe7da32d2c1272b19ac8c
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-03 22:38:51 +0000

    added pep8 fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,22 +10,24 @@ if __name__ == "__main__":
     import requests as req
     from sys import argv
 
-
     user_id = argv[1]
 
-    user_dict = req.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
-    todo_list = req.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)).json()
+    user_dict = req.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(user_id)).json()
+    todo_list = req.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
+                        format(user_id)).json()
 
     completed_tasks = []
     total_tasks = len(todo_list)
     total_complete = 0
-    
+
     for task in todo_list:
-        if task['completed'] == True:
+        if task['completed'] is True:
             total_complete += 1
             completed_tasks.append(task['title'])
 
-    print('Employee {} is done with tasks({}/{}):'.format(user_dict['name'], total_complete, total_tasks))
-    
+    print('Employee {} is done with tasks({}/{}):'.
+          format(user_dict['name'], total_complete, total_tasks))
+
     for task in completed_tasks:
         print('\t', task)
