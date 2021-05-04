c7061c1d6a9f64d707dba15313da2e2966f8bdef
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-04 00:51:20 +0000

    adding task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Description:
+    A Python script that, using this REST API
+(https://intranet.hbtn.io/rltoken/0Ltm_dXy-m4E9jchBrKLVA),
+for a given employee ID, returns information about
+his/her TODO list progress.
+
+Also exports data in the JSON format...
+"""
+if __name__ == "__main__":
+    import requests as req
+    from sys import argv
+    import json
+
+    user_dict = req.get('https://jsonplaceholder.typicode.com/users').json()
+    print_dict = {}
+
+    for user in user_dict:
+        user_id = user['id']
+        todo_list = req.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
+                            format(user_id)).json()
+        username = user['username']
+
+        print_list = []
+
+        for task in todo_list:
+            task_info = {}
+            task_info['task'] = task['title']
+            task_info['completed'] = task['completed']
+            task_info['username'] = username
+            print_list.append(task_info)
+        print_dict['{}'.format(user_id)] = print_list
+
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(print_dict, f)
