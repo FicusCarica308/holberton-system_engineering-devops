07380ea7f3f122f0f6e1d70c530e8fad5c8e5545
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-19 03:20:34 +0000

    added task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-""" """
+"""Module contains singular function"""
 import requests
+import json
+
 
 def number_of_subscribers(subreddit):
-    """ """
-    subs = requests.get('http://www.reddit.com/r/{}/about'.format(subreddit))
-    print(subs.content)
+    """Finds the amount of subscribers from a given subreddit"""
+    headers = requests.utils.default_headers()
+    headers.update(
+        {
+            'User-Agent': 'MannyAgent',
+        }
+    )
+    subs = requests.get('https://www.reddit.com/r/{}/about.json'.
+                        format(subreddit))
+    if subs.reason != 'OK':
+        return 0
+    resp = subs.json()
+    if 'data' in resp:
+        return (subs.json()['data']['subscribers'])
+    else:
+        return (0)
