e6210f377ce892ca0ca67953eaeb61edc03c2938
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-19 03:29:43 +0000

    added prenthesis to both returns

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     subs = requests.get('https://www.reddit.com/r/{}/about.json'.
                         format(subreddit))
     if subs.reason != 'OK':
-        return (0)
+        return 0
     resp = subs.json()
     if 'data' in resp:
         return (subs.json()['data']['subscribers'])
