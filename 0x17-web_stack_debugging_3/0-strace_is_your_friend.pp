5761b54e3128d049b0702c559db1d5bed8c5b58d
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-22 15:52:44 +0000

    added task 0 version without file_line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # This manifest will fix a specific error in a given container
-file_line {'fix-error':
-    path  => '/var/www/html/wp-settings.php',
-    match => '^require_once\( ABSPATH . WPINC . \'/class-wp-locale.phpp\' \);',
-    line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+exec { 'modify_file':
+    command => "sed -i '/.phpp/ s/phpp/php/' '/var/www/html/wp-settings.php'",
+    path    => '/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
